18025b52c9979109be6b1b0a38179d901429fc44
Holden Wiese <wieseholden@gmail.com>
2020-08-06 15:46:42 -0600

    Add John to Db.

---
+++ CodeFirstFormsApp/Migrations/202008061914162_Add John T.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeFirstFormsApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddJohnT : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
